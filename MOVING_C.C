29e04d0a941029b001f584c473de00ba8c16f371
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2019-09-02 12:04:16 +0530

    Add files via upload

---
+++ MOVING_C.C
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+#include<dos.h>
+void main(){
+int gd=DETECT,gm;
+int i,maxx,midy;
+initgraph(&gd,&gm,"C:\\TurboC3\\BGI");
+maxx=getmaxx();
+midy=getmaxy()/2;
+for(i=0;maxx+5;i=i+3){
+cleardevice();
+setcolor(WHITE);
+setfillstyle(SOLID_FILL,RED);
+line(i,midy+23,i,midy);
+line(i,midy,40+i,midy-20);
+line(40+i,midy-20,80+i,midy-20);
+line(80+i,midy-20,100+i,midy);
+line(120+i,midy,120+i,midy+23);
+line(100+i,midy,120+i,midy);
+line(0+i,midy+23,18+i,midy+23);
+arc(30+i,midy+23,0,180,12);
+line(42+i,midy+23,78+i,midy+23);
+arc(90+i,midy+23,0,180,12);
+line(102+i,midy+23,120+i,midy+23);
+line(28+i,midy,43+i,midy-15);
+line(43+i,midy-15,57+i,midy-15);
+line(57+i,midy-15,57+i,midy);
+line(57+i,midy,28+i,midy);
+line(62+i,midy-15,77+i,midy-15);
+line(77+i,midy-15,92+i,midy);
+line(92+i,midy,62+i,midy);
+line(62+i,midy,62+i,midy-15);
+setcolor(BLUE);
+setfillstyle(SOLID_FILL,DARKGRAY);
+circle(90+i,midy+25,9);
+circle(30+i,midy+25,9);
+floodfill(30+i,midy+25,BLUE);
+floodfill(90+i,midy+25,BLUE);
+delay(10);
+}
+getch();
+closegraph();
+}
